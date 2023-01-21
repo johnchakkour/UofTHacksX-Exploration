ddde67d611f4598a6f894d747072146ff5746606
Irha Ali <irhaali_97@hotmail.ca>
2023-01-21 01:15:23 -0500

    Add routes on server page for now

--- server/index.js
+++ server/index.js
@@ -8,8 +8,18 @@ app.set("view engine", "ejs");
 const PORT = 8080; // default port 8080
 
 // Main page
-app.get("/", (req, res) => {
-  res.send('Hello');
+app.get("/", function(req, res) {
+  res.send('You cam to the main page');
+});
+
+// Add an event
+app.get("/add", function(req, res) {
+  res.send('You came to make a post');
+});
+
+// Edit/remore an event
+app.get("/edit", function(req, res) {
+  res.send('You came to remove/edit a post');
 });
 
 // Start listening
